f23c6c4025ca6fd3a5a4ee27c812877bf2d279bc
Remim7 <1197097889@qq.com>
2018-04-27 07:39:39 +0800

    Update mokee.mk

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/sm/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
@@ -7,5 +7,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
-PRODUCT_NAME := mk_honami
+PRODUCT_NAME := sm_honami
 PRODUCT_DEVICE := honami
